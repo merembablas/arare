1c1ff6986d393a608736a6545a58722182df8ba8
robin <robin@ansvia.com>
2021-08-25 23:50:42 +0700

    first init

---
+++ components/Tutorial.vue
@@ -0,0 +1,19 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.1.2/dist/tailwind.min.css" rel="stylesheet">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <div class="mt-8 bg-white overflow-hidden shadow sm:rounded-lg p-6">
+        <h2 class="text-2xl leading-7 font-semibold">
+          Welcome to Arare
+        </h2>
+        <p class="mt-3 text-gray-600">
+          Arare is decentralized NFT marketplace running on <a href="https://nuchain.network">nuchain.network</a>
+        </p>
+        <p class="mt-4 justify-center text-center content-center pt-4 text-gray-800 border-t border-dashed">
+          coming soon
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+export default {}
+</script>
+
+
+<style lang="less">
+a {
+    color: rgb(72, 130, 255) !important;
+}
+</style>
