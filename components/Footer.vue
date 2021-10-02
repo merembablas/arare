c726c06ff8ba32e707952469a4637d43ae554266
robin <robin@ansvia.com>
2021-10-02 16:04:40 +0700

    improve(UI): Footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,14 +3,15 @@
     class="
       bg-white
       pb-10
-      flex flex-auto
+      flex flex-wrap
+      md:flex-auto
       pt-5
       border-t-2
-      justify-between
+      md:justify-between
       items-end
     "
   >
-    <div class="m-10 footer flex flex-col w-64">
+    <div class="m-5 md:m-10 footer flex flex-col w-64">
       <div class="flex items-center">
         <Logo width="32" height="32" />
         <div class="ml-2 font-semibold text-3xl">arare</div>
@@ -19,7 +20,19 @@
       <div></div>
       <div>Copyright &copy; <a href="http://arare.one">arare.one</a></div>
     </div>
-    <div class="m-10 footer flex flex-wrap items-center justify-center w-64">
+    <div
+      class="
+        m-5
+        md:m-10
+        footer
+        flex flex-wrap
+        items-start
+        md:items-center
+        justify-start
+        md:justify-center
+        w-64
+      "
+    >
       <div>Powered by</div>
       <a class="ml-2" href="https://nuchain.network" target="_blank"> </a>
       <img
